e62dcba62c31ff51c799cc85f63e246661d601ca
Ari Bernstein <aribernstein1997@gmail.com>
2024-01-29 18:38:35 -0500

    user and profile creation

--- finstagramproj/finstagramapp/models.py
+++ finstagramproj/finstagramapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    uid = models.IntegerField()
+    bio = models.TextField(max_length=1000, blank=True)
+    profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username}'
--- finstagramproj/finstagramapp/views.py
+++ finstagramproj/finstagramapp/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
+
+from .models import Profile
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password != password2:
+            messages.info(request, 'Passwords do not match!!')
+            return redirect('signup')
+        
+        if User.objects.filter(email=email).exists():
+            messages.info(request, 'Email already exists!!')
+            return redirect('signup')
+        
+        if User.objects.filter(username=username).exists():
+            messages.info(request, 'Username is taken :(')
+            return redirect('signup')
+        
+        user = User.objects.create_user(
+            username=username, password=password, email=email
+        )
+        user.save()
+
+        user_model = User.objects.get(username=username)
+        user_profile = Profile.objects.create(user=user_model, uid=user_model.id)
+        user_profile.save()
+        return redirect('login')
+
+    return render(request, 'signup.html')
