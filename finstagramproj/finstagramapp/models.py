16d24df45e1af86644f68a800bc10ed0c202df00
Ari Bernstein <aribernstein1997@gmail.com>
2024-01-29 23:43:16 -0500

    updating profiles now works

--- finstagramproj/finstagramapp/models.py
+++ finstagramproj/finstagramapp/models.py
@@ -7,6 +7,8 @@ User = get_user_model()
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     uid = models.IntegerField()
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
     bio = models.TextField(max_length=1000, blank=True)
     profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=True)
--- finstagramproj/finstagramapp/views.py
+++ finstagramproj/finstagramapp/views.py
@@ -13,9 +13,33 @@ def index(request):
 
 
 @login_required(login_url='signin')
-def signout(request):
-    auth.logout(request)
-    return redirect('signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    
+    if request.method == 'POST':
+
+        bio = request.POST['bio']
+        location = request.POST['location']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+
+        if request.FILES.get('profile_img') is None:
+            image = user_profile.profile_img
+        else:
+            image = request.FILES.get('profile_img')
+        
+        user_profile.profile_img = image
+        user_profile.bio = bio
+        user_profile.location = location
+        user_profile.first_name = first_name
+        user_profile.last_name = last_name 
+        user_profile.save()
+
+        return redirect('settings')
+
+            
+
+    return render(request, 'settings.html', {'user_profile': user_profile})
 
 
 def signup(request):
@@ -37,15 +61,22 @@ def signup(request):
             messages.info(request, 'Username is taken :(')
             return redirect('signup')
         
+
+        # Create user & profile
         user = User.objects.create_user(
             username=username, password=password, email=email
         )
         user.save()
-
+        
+        # Create profile
         user_model = User.objects.get(username=username)
         user_profile = Profile.objects.create(user=user_model, uid=user_model.id)
         user_profile.save()
-        return redirect('signin')
+
+        # Login user and redirect them to settings page to update their profile
+        user_login = auth.authenticate(username=username, password=password)
+        auth.login(request, user_login)
+        return redirect('settings')
 
     return render(request, 'signup.html')
 
@@ -66,3 +97,9 @@ def signin(request):
 
 
     return render(request, 'signin.html')
+
+
+@login_required(login_url='signin')
+def signout(request):
+    auth.logout(request)
+    return redirect('signin')
